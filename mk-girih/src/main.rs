246e72b2cfc57e1876fc7a522a33a14b04ff1d1c
James Adam Buckland <james.adam.buckland@gmail.com>
2023-09-25 16:42:55 -0400

    collapse

--- mk-girih/src/main.rs
+++ mk-girih/src/main.rs
@@ -29,53 +29,28 @@ fn main() {
 
     let margin = 25.0;
 
-    // let mut so2s: Vec<StyledObj2> = geom::all_girih_tiles_in_random_order()
-    //     .iter()
-    //     .map(|g| (*g, g.color()))
-    //     .into_iter()
-    //     .flat_map(|(girih_enum, color)| {
-    //         let t = geom::Tile::new(girih_enum);
-    //         let tile = t.to_pg2();
-    //         let strapwork = t.to_strapwork();
-
-    //         let stripes =
-    //             shade_polygon(&ShadeConfig::builder().gap(0.05).slope(0.05).build(), &tile)
-    //                 .unwrap()
-    //                 .into_iter()
-    //                 .map(|stripe| {
-    //                     StyledObj2::new(stripe)
-    //                         .with_thickness(0.1)
-    //                         .with_color(color)
-    //                 });
-    //         let outline = StyledObj2::new(tile).with_style(Style::new(&color, 2.0));
-    //         let straps = strapwork
-    //             .into_iter()
-    //             .map(|strap| StyledObj2::new(strap).with_thickness(2.0).with_color(color));
-    //         stripes.chain(std::iter::once(outline)).chain(straps)
-    //     })
-    //     .collect::<Vec<_>>();
-
-    let so2s = strategy2::run(&strategy2::Settings {
-        num_iterations: 15,
-        is_deterministic: true,
-    })
-    .map(|mut so2| {
-        so2 *= 20.0;
-        so2 += Pt2(200, 200);
-        so2
-    })
-    .collect::<Vec<_>>();
-
-    Canvas::from_objs(so2s, /*autobucket=*/ true)
-        .with_frame(make_frame(
-            /*wh=*/ (800.0 - 2.0 * margin, 1000.0 - 2.0 * margin),
-            /*offset=*/ p2!(margin, margin),
-        ))
-        .write_to_svg_or_die(
-            Size {
-                width: 1000,
-                height: 800,
-            },
-            &args.output_path_prefix,
-        );
+    Canvas::from_objs(
+        strategy2::run(&strategy2::Settings {
+            num_iterations: 20,
+            is_deterministic: false,
+        })
+        .map(|mut so2| {
+            so2 *= 20.0;
+            so2 += Pt2(200, 200);
+            so2
+        }),
+        /*autobucket=*/
+        true,
+    )
+    .with_frame(make_frame(
+        /*wh=*/ (800.0 - 2.0 * margin, 1000.0 - 2.0 * margin),
+        /*offset=*/ p2!(margin, margin),
+    ))
+    .write_to_svg_or_die(
+        Size {
+            width: 1000,
+            height: 800,
+        },
+        &args.output_path_prefix,
+    );
 }
