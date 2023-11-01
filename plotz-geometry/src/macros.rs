8c8b94bd5394dd7da529b24ea24bb6406183b839
James Adam Buckland <james.adam.buckland@gmail.com>
2023-11-01 18:41:01 -0400

    ops_defaults for pg

--- plotz-geometry/src/macros.rs
+++ plotz-geometry/src/macros.rs
@@ -1,43 +1,55 @@
 //! Macros.
 #![allow(missing_docs)]
 
+/// impl<T> Trait<T> for Name
 #[macro_export]
 macro_rules! ops_generic {
     ($name:ident, $trait:ident, $fn:ident) => {
         impl<T> $trait<T> for $name
         where
-            T: Into<Pt> + Copy,
+            T: Into<Pt>,
         {
             type Output = Self;
             fn $fn(self, rhs: T) -> Self::Output {
                 let mut x = self.clone();
-                x.iter_mut().for_each(|pt| *pt = pt.$fn(rhs.into()));
-                x
-            }
-        }
-
-        impl<T> $trait<T> for &$name
-        where
-            T: Into<Pt> + Copy,
-        {
-            type Output = $name;
-            fn $fn(self, rhs: T) -> Self::Output {
-                let mut x = self.clone();
-                x.iter_mut().for_each(|pt| *pt = pt.$fn(rhs.into()));
+                let rhs: Pt = rhs.into();
+                x.iter_mut().for_each(|pt| *pt = pt.$fn(rhs));
                 x
             }
         }
     };
 }
+
+/// impl<T> Trait<T> for &Name
+#[macro_export]
+macro_rules! ops_generic_ref {
+    ($name:ident, $trait:ident, $fn:ident) => {
+        impl<T> $trait<T> for &$name
+        where
+            T: Into<Pt>,
+        {
+            type Output = $name;
+            fn $fn(self, rhs: T) -> Self::Output {
+                let mut x = self.clone();
+                let rhs: Pt = rhs.into();
+                x.iter_mut().for_each(|pt| *pt = pt.$fn(rhs));
+                x
+            }
+        }
+    };
+}
+
+/// impl<T> TraitAssign<T> for Name
 #[macro_export]
 macro_rules! ops_assign_generic {
     ($name:ident, $trait:ident, $fn:ident) => {
         impl<T> $trait<T> for $name
         where
-            T: Into<Pt> + Copy,
+            T: Into<Pt>,
         {
             fn $fn(&mut self, rhs: T) {
-                self.iter_mut().for_each(|pt| pt.$fn(rhs.into()));
+                let rhs: Pt = rhs.into();
+                self.iter_mut().for_each(|pt| pt.$fn(rhs));
             }
         }
     };
@@ -50,6 +62,10 @@ macro_rules! ops_defaults {
         $crate::ops_generic!($name, Div, div);
         $crate::ops_generic!($name, Mul, mul);
         $crate::ops_generic!($name, Sub, sub);
+        $crate::ops_generic_ref!($name, Add, add);
+        $crate::ops_generic_ref!($name, Div, div);
+        $crate::ops_generic_ref!($name, Mul, mul);
+        $crate::ops_generic_ref!($name, Sub, sub);
         $crate::ops_assign_generic!($name, AddAssign, add_assign);
         $crate::ops_assign_generic!($name, DivAssign, div_assign);
         $crate::ops_assign_generic!($name, SubAssign, sub_assign);
