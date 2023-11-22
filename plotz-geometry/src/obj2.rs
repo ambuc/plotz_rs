38d1e6f28065626831841467d4ae176af9a31022
James Adam Buckland <james.adam.buckland@gmail.com>
2023-11-22 16:18:20 -0500

    simplify Croppable::crop for obj2

--- plotz-geometry/src/obj2.rs
+++ plotz-geometry/src/obj2.rs
@@ -5,6 +5,7 @@ use crate::{
     bounded::{Bounded, Bounds},
     crop::{CropType, Croppable, PointLocation},
     group::Group,
+    overlaps::{polygon_overlaps_point, polygon_overlaps_segment},
     shapes::{
         curve::CurveArc, multiline::Multiline, point::Point, polygon::Polygon,
         polygon_with_cavity::PolygonWithCavities, segment::Segment, text::Text,
@@ -49,57 +50,26 @@ crate::ops_defaults_t!(Obj2, Point);
 impl Croppable for Obj2 {
     type Output = Obj2;
     fn crop(&self, frame: &Polygon, crop_type: CropType) -> Result<Vec<Self::Output>> {
-        Ok(match &self {
-            Obj2::Point(pt) => {
-                assert_eq!(crop_type, CropType::Inclusive);
-                if !matches!(frame.contains_pt_deprecated(pt), Ok(PointLocation::Outside)) {
-                    vec![self.clone()]
-                } else {
-                    vec![]
-                }
-            }
-            Obj2::Multiline(ml) => ml
-                .to_segments()
-                .into_iter()
-                .map(|sg| sg.crop(frame, crop_type))
-                .flatten_ok()
-                .collect::<Result<Vec<_>>>()?
-                .into_iter()
-                .map(Obj2::from)
-                .collect::<Vec<_>>(),
-            Obj2::Polygon(pg) => pg
+        match &self {
+            Obj2::Point(p) => match polygon_overlaps_point(frame, p)? {
+                Some((_, p)) => Ok(vec![Obj2::Point(p)]),
+                None => Ok(vec![]),
+            },
+            Obj2::Segment(sg) => match polygon_overlaps_segment(frame, sg)? {
+                Some((_, sgops)) => Ok(sgops.into_iter().map(|sgop| sgop.to_obj(sg)).collect()),
+                None => todo!(),
+            },
+            Obj2::Multiline(_ml) => todo!("when polygon_overlaps_multiline is implemented."),
+            Obj2::Polygon(pg) => Ok(pg
                 .crop(frame, crop_type)?
                 .into_iter()
                 .map(Obj2::from)
-                .collect::<Vec<_>>(),
-            Obj2::PolygonWithCavities(_) => unimplemented!("TODO: implement cropping for Pgc."),
-            Obj2::Segment(sg) => sg
-                .crop(frame, crop_type)?
-                .into_iter()
-                .map(Obj2::from)
-                .collect::<Vec<_>>(),
-            Obj2::CurveArc(ca) => ca
-                .crop(frame, crop_type)?
-                .into_iter()
-                .map(Obj2::from)
-                .collect::<Vec<_>>(),
-            Obj2::Text(ch) => {
-                assert_eq!(crop_type, CropType::Inclusive);
-                if !matches!(
-                    frame.contains_pt_deprecated(&ch.pt),
-                    Ok(PointLocation::Outside)
-                ) {
-                    vec![self.clone()]
-                } else {
-                    vec![]
-                }
-            }
-            Obj2::Group(g) => g
-                .crop(frame, crop_type)?
-                .into_iter()
-                .map(Obj2::from)
-                .collect::<Vec<_>>(),
-        })
+                .collect::<Vec<_>>()),
+            Obj2::PolygonWithCavities(_) => todo!(),
+            Obj2::CurveArc(_) => todo!(),
+            Obj2::Text(_) => todo!(),
+            Obj2::Group(_) => todo!(),
+        }
     }
 
     fn crop_excluding(&self, other: &Polygon) -> Result<Vec<Self::Output>>
